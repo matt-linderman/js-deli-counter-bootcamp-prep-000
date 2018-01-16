afc7c16b27b1e489d4e4db21fcdb2d15159ae085
Matthew Linderman <mwl53@mit.edu>
2018-01-16 18:31:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,21 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
-      var firstInLine = katzDeliLine[0]
-      katzDeliLine.shift()
-      return `Currently serving ${firstInLine}.`
+    var firstInLine = katzDeliLine[0]
+    katzDeliLine.shift()
+    return `Currently serving ${firstInLine}.`
   }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  } else {
+    var lineDesc = "The line is currently:"
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      lineDesc += ` ${i+1}. ${katzDeliLine[i]},`
+    }
+    lineDesc.length = lineDesc.length - 1
+  }
+  
 }
